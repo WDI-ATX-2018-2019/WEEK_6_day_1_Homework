08b603dde904ae20a8069ef0e53df7bb3d98af4d
pittpir <pittpir@gmail.com>
2019-01-22 20:27:23 -0600

    Create and Initialize files

---
+++ customers/models.js
@@ -0,0 +1,45 @@
+const customers = [
+  {
+    name: 'Cassie',
+    age: 19,
+    sex: 'Female',
+    likes: 'makeup'
+  },
+  {
+    name: 'Christopher',
+    age: 21,
+    sex: 'Male',
+    likes: 'video games'
+  },
+  {
+    name: 'Ava',
+    age: 6,
+    sex: 'Female',
+    likes: 'LOL'
+  },
+  {
+    name: 'Liliana',
+    age: 4,
+    sex: 'Female',
+    likes: 'barbies'
+  },
+  {
+    name: 'Judy',
+    age: 25,
+    sex: 'Female',
+    likes: 'crafts'
+  },
+  {
+    name: 'Liam',
+    age: 9,
+    sex: 'Male',
+    likes: 'minecraft'
+  },
+  {
+    name: 'Freya',
+    age: 1,
+    sex: 'Female',
+    likes: 'food'
+  }
+]
+module.exports = customers;
---
+++ products/models.js
@@ -0,0 +1,45 @@
+const products = [
+  {
+    productName: 'Makeup',
+    cost: 19,
+    color: 'beige',
+    onSale: false
+  },
+  {
+    productName: 'Barbies',
+    cost: 15,
+    color: 'blue',
+    onSale: true
+  },
+  {
+    productName: 'HoTs',
+    cost: 20,
+    color: 'blue',
+    onSale: true
+  },
+  {
+    productName: 'Minecraft',
+    cost: 25,
+    color: 'green',
+    onSale: false
+  },
+  {
+    productName: 'Sewing Machine',
+    cost: 100,
+    color: 'white',
+    onSale: true
+  },
+  {
+    productName: 'Baby Food',
+    cost: 21,
+    color: 'yellow',
+    onSale: false
+  },
+  {
+    productName: 'LOL',
+    cost: 10,
+    color: 'pink',
+    onSale: false
+  }
+]
+module.exports = products;
